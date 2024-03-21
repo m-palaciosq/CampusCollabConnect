8bc0a5b8b227825f3d6ecf239c7e573b111f12f0
Martin Palacios <m_palaciosq@uncg.edu>
2024-03-21 14:30:10 -0400

    made changes to account creation

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
-    $('#accountCreationForm').submit(function(e) {
-        e.preventDefault();
-        
-        // Fetch input values
-        var newEmail = $('#newEmail').val();
-        var newPassword = $('#newPassword').val();
-        var confirmPassword = $('#confirmPassword').val();
-
-        // Checks
-        if(!newEmail.endsWith('@uncg.edu') && newPassword !== confirmPassword){
-            $('#creation-error-message').text('Email must end with @uncg.edu\n Passwords do not match');
-            return;
-        }else if (newPassword !== confirmPassword) {
-            $('#creation-error-message').text('Passwords do not match');
-            return;
-        }else if(!newEmail.endsWith('@uncg.edu')){
-            $('#creation-error-message').text('Email must end with @uncg.edu');
-            return;
-        }
-        // can perform more validation like checking username uniqueness, password strength, etc.
-
-        window.location.href = "login.html";
-    });
-});
-$(document).ready(function() {
     $('#create-project-form').submit(function(event) {
         event.preventDefault(); // Prevent form submission
 
