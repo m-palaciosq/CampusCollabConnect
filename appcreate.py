da98aa7968b104a1fa4d18e83ab3e35377ecb337
Wesley Nguyen <w_nguyen@uncg.edu>
2024-03-27 20:53:45 -0400

    Update appcreate.py
    reverted port number from 3306 -> 3307

--- appcreate.py
+++ appcreate.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def get_db_connection():
     connection = mysql.connector.connect(
         host='127.0.0.1',  
-        port='3306',       
+        port='3307',       
         user='root',
         password='',      
         database='campuscollabconnect'
